cc5a53bd833681ffabd395a1e6ff4445e8c12ada
terrytsan <terrytsan@hotmail.com>
2021-01-27 12:13:13 +0000

    Focus is Now Correctly Set After Opening a Modal
    - When opening a modal, the first text box is now set as the focus.

--- src/components/EditItemModal.js
+++ src/components/EditItemModal.js
@@ -1,26 +1,36 @@
 import {Button, Form, Modal} from "react-bootstrap";
+import React, {Component} from "react";
 
-function EditItemModal({
-						   modalShow, modalClose, editItemName, handleNameChange, editItemClick, errors
-					   }) {
-	return (
-		<Modal show={modalShow} onHide={modalClose} animation={false}>
-			<Modal.Header closeButton={true}>
-				<Modal.Title>Edit Item</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form>
-					<Form.Group>
-						<Form.Control type="text" placeholder="Item Name" value={editItemName}
-									  onChange={handleNameChange} isInvalid={!!errors.itemName}/>
-						<Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
-					</Form.Group>
-					<Button type="submit" onClick={editItemClick}>Edit</Button>
-				</Form>
-			</Modal.Body>
-		</Modal>
+class EditItemModal extends Component {
 
-	);
+	constructor(props, context) {
+		super(props, context);
+		this.txtItmName = React.createRef();
+	}
+
+	render() {
+		let {
+			modalShow, modalClose, editItemName, handleNameChange, editItemClick, errors
+		} = this.props;
+		return (
+			<Modal show={modalShow} onHide={modalClose} animation={false}
+				   onShow={() => this.txtItmName.current.focus()}>
+				<Modal.Header closeButton={true}>
+					<Modal.Title>Edit Item</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group>
+							<Form.Control type="text" ref={this.txtItmName} placeholder="Item Name" value={editItemName}
+										  onChange={handleNameChange} isInvalid={!!errors.itemName}/>
+							<Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
+						</Form.Group>
+						<Button type="submit" onClick={editItemClick}>Edit</Button>
+					</Form>
+				</Modal.Body>
+			</Modal>
+		);
+	}
 }
 
 export default EditItemModal;
--- src/components/NewItemModal.js
+++ src/components/NewItemModal.js
@@ -1,30 +1,41 @@
 import {Button, Form, Modal} from "react-bootstrap";
+import React, {Component} from "react";
 
-function NewItemModal({
-						  modalShow, modalClose, newItemName, handleNameChange, newItemHighPriority,
-						  handlePriorityChange, addItemClick, errors
-					  }) {
-	return (
-		<Modal show={modalShow} onHide={modalClose} animation={false}>
-			<Modal.Header closeButton={true}>
-				<Modal.Title>New Item</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form>
-					<Form.Group>
-						<Form.Control type="text" placeholder="Item Name" value={newItemName}
-									  onChange={handleNameChange} isInvalid={!!errors.itemName}/>
-						<Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
-					</Form.Group>
-					<Form.Group>
-						<Form.Check label="High Priority" checked={newItemHighPriority}
-									onChange={handlePriorityChange}/>
-					</Form.Group>
-					<Button type="submit" onClick={addItemClick}>Add</Button>
-				</Form>
-			</Modal.Body>
-		</Modal>
-	);
+class NewItemModal extends Component {
+
+	constructor(props, context) {
+		super(props, context);
+		this.txtItmName = React.createRef();
+	}
+
+	render() {
+		let {
+			modalShow, modalClose, newItemName, handleNameChange, newItemHighPriority,
+			handlePriorityChange, addItemClick, errors
+		} = this.props;
+		return (
+			<Modal show={modalShow} onHide={modalClose} animation={false}
+				   onShow={() => this.txtItmName.current.focus()}>
+				<Modal.Header closeButton={true}>
+					<Modal.Title>New Item</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group>
+							<Form.Control type="text" ref={this.txtItmName} placeholder="Item Name" value={newItemName}
+										  onChange={handleNameChange} isInvalid={!!errors.itemName}/>
+							<Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
+						</Form.Group>
+						<Form.Group>
+							<Form.Check label="High Priority" checked={newItemHighPriority}
+										onChange={handlePriorityChange}/>
+						</Form.Group>
+						<Button type="submit" onClick={addItemClick}>Add</Button>
+					</Form>
+				</Modal.Body>
+			</Modal>
+		);
+	}
 }
 
 export default NewItemModal;
